af5b576913327ec9bcf17a8423f6e547df3901da
Fabrizio Monti <fabrizio.monti@welaika.com>
2017-07-25 11:39:29 +0200

    Add original deface hash

--- app/overrides/add_bank_details_to_order_show.rb
+++ app/overrides/add_bank_details_to_order_show.rb
@@ -2,5 +2,6 @@ Deface::Override.new(
   virtual_path: 'spree/shared/_order_details',
   name: 'add_bank_details_to_order_show',
   insert_bottom: '.payment-info',
-  partial: 'spree/orders/bank_transfer_details'
+  partial: 'spree/orders/bank_transfer_details',
+  original: 'c9c8e91826a4fe1acd3dcee37427dd74eccd7015'
 )
