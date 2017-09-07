10857f83f1665d0f83b1fbf3bf08a5d318660081
Fabrizio Monti <fabrizio.monti@welaika.com>
2017-09-07 12:06:45 +0200

    Remove originals sha

--- app/overrides/add_bank_details_to_confirm_email.rb
+++ app/overrides/add_bank_details_to_confirm_email.rb
@@ -2,6 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/order_mailer/confirm_email',
   name: 'add_bank_details_to_confirm_email',
   insert_bottom: "[data-hook='payment-method']",
-  original: 'fe50b25a81d284982f52da6761da05f7b8d5e121',
   partial: 'spree/order_mailer/bank_transfer_details'
 )
--- app/overrides/add_bank_details_to_order_show.rb
+++ app/overrides/add_bank_details_to_order_show.rb
@@ -2,6 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/shared/_order_details',
   name: 'add_bank_details_to_order_show',
   insert_bottom: '.payment-info',
-  original: 'c9c8e91826a4fe1acd3dcee37427dd74eccd7015',
   partial: 'spree/orders/bank_transfer_details'
 )
